b58a2b684a08b5d483806772943c4506882bad3f
BlackBelt1004 <71718915+BlackBelt1004@users.noreply.github.com>
2021-10-19 13:06:28 -0500

    Add files via upload

---
+++ MPG.java
@@ -0,0 +1,42 @@
+package mpg;
+import java.util.Scanner;
+//import java.lang.Math;
+import java.text.NumberFormat;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class MPG {
+	  public static void main(String[] args) {
+	        System.out.println("Welcome to the Miles Per Gallon calculator");
+	        System.out.println();  // print a blank line
+	        
+	        @SuppressWarnings("resource")
+			Scanner sc = new Scanner(System.in);
+	        
+	        String choice = "y";
+	        while (choice.equalsIgnoreCase("y")) {
+	            System.out.print("Enter miles driven: ");
+	            //double miles = sc.nextDouble();
+	            BigDecimal miles = new BigDecimal(sc.next());
+	            System.out.print("Enter gallons of gas used: ");
+	            //double gallons = sc.nextDouble();
+	            BigDecimal gallons = new BigDecimal(sc.next());
+	           // double mpg = miles/gallons;
+	            BigDecimal mpg = miles.divide(gallons, 2, RoundingMode.HALF_UP);
+	            NumberFormat number = NumberFormat.getNumberInstance();
+	            number.setMaximumFractionDigits(2);
+	            
+	            //mpg = (int)Math.round(mpg);
+	            
+	            //mpg = (double)Math.round(mpg*100)/100;
+	            //System.out.println("Miles per gallon is " + number.format(mpg) + ".");
+	            System.out.println("Miles per gallon is " + mpg.toString() + ".");
+	            //System.out.println("Miles per gallon is " + mpg + ".");
+	            System.out.println(); 
+	            
+	            System.out.print("Calculate another MPG? (y/n): ");
+	            choice = sc.next();
+	            System.out.println();
+	        }
+	    }
+	}    
